9ae175c52cf8ca87467b2eb6ad389f2f8a937855
onofflink <48428378+onofflink@users.noreply.github.com>
2020-09-22 15:21:48 +0900

    add bs4bsjobsearch.py

---
+++ 922022bsjobsearch.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+# download wikipage
+jobsearchUrl = "https://www.google.com/search?sxsrf=ALeKk03e98mgrZzyS-zhBj0F09ts0Gr_1A%3A1600752239063&ei=b4ppX7ezA6WSr7wP3b2jwA8&q=%EC%B7%A8%EC%97%85%EC%82%AC%EC%9D%B4%ED%8A%B8&oq=%EC%B7%A8%EC%97%85%EC%82%AC%EC%9D%B4%ED%8A%B8&gs_lcp=CgZwc3ktYWIQAzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQR1AAWABgnIUQaABwAngAgAEAiAEAkgEAmAEAqgEHZ3dzLXdpesgBCMABAQ&sclient=psy-ab&ved=0ahUKEwj3ptTkgvzrAhUlyYsBHd3eCPgQ4dUDCA0&uact=5"
+result = requests.get(jobsearchUrl, allow_redirects=True)
+print(result)
+
+#open(result, 'wb').write(result.cotent)
+
+if result.status_code == 200:
+    soup = BeautifulSoup(result.content, "lxml")
+
+parameters = soup.findAll(name='a')
